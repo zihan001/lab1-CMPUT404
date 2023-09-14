a691546e395f503004c68024bd3cb5ee09dbe354
“Zihan <zihanhossain99@gmail.com>
2023-09-13 18:13:02 -0600

    final commit for last question

--- versionfinder.py
+++ versionfinder.py
@@ -2,4 +2,8 @@ import requests
 
 print(requests.__version__)
 
-print(requests.get("http://google.com/"))
+google_resp = requests.get("http://google.com/")
+print(google_resp.text)
+
+my_resp = requests.get("https://raw.githubusercontent.com/zihan001/lab1-CMPUT404/main/versionfinder.py")
+print(my_resp.text)
